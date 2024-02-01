95f455563b49c92d1450d9d0ff1c20e735e71342
unknown <g.pirveli22@.gmail.com>
2024-02-01 14:08:15 +0400

    Fo

---
+++ new.php
@@ -0,0 +1,43 @@
+<?php
+
+
+
+
+// if ($_POST["Giorgi" ]=='Giorgi') {
+//     echo 'yes';
+// } else {
+//     echo 'no';
+// }
+
+
+
+// if ($_POST["Pirveli" ]=='Pirveli') {
+//     echo 'yes';
+// } else {
+//     echo 'no';
+// }
+
+
+
+
+// if ($_POST["Pop" ]=='Pirveli') {
+//     echo 'yes';
+// } else {
+//     echo 'no';
+// }
+
+
+
+if ($_POST["Giorgi" ]=='Giorgi'&& $_POST["Pirveli" ]=='Pirveli' && $_POST["Pop" ]=='Pirveli') {
+    echo "თქვენ წარმატებით დარეგისტრირდით";
+} else {
+    echo 'პაროლი და მისი
+    კონფირმაცია არასწორია';
+}
+
+
+
+
+
+
+
